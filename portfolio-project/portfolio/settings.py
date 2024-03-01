f36a380ed3d3f91dca27c7fc0f37547938da1704
stijn <stijnsepter@gmail.com>
2024-03-01 12:41:36 +0100

    allen nog foto's en teksten toevoegen

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -121,6 +121,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+SILENCED_SYSTEM_CHECKS = ["auth.W004"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
