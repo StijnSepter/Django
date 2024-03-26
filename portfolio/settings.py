98b1c6462a529e575dc8cd6f93f809b21dbdd23a
stijn <stijnsepter@gmail.com>
2024-03-26 12:09:47 +0100

    ;kljfd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,8 +41,7 @@ SECRET_KEY = 'django-insecure-h*m89913fgv%t8yr7_@0$@n$66*u4$riukcth5)3-c+00p7z!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.38.128.183', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
